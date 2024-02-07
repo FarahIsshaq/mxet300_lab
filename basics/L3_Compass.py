ef22b7172ba429841c1f6a89a6aa29db252078d1
FarahIsshaq <isshaqfarah@gmail.com>
2024-02-07 16:09:09 -0600

    Lab Three Changes Made

--- basics/L3_Compass.py
+++ basics/L3_Compass.py
@@ -1,21 +1,21 @@
-
-
-from L1_ina import readVolts
-from L3_Compass import Cardinal
 from L2_compass_heading import get_heading
-import csv
 import time
+import csv
 from L1_log import tmpFile
 from L1_log import stringTmpFile
 
 
+def Cardinal():
+    Degrees = int(get_heading())
+    directions = ['North', 'North West', 'West', 'South West', 'South', 'South East', 'East', 'North East', 'North']
+    index = round(Degrees / 45) % 8  
+    return directions[index]
+
+
 if __name__ == "__main__":
     while True:
-        myBatt = round(readVolts(),2)       # collect a reading
-        print("Battery Voltage: ",myBatt)   # print the reading
-        tmpFile(myBatt, "VoltageTempFile")
         CD = Cardinal()
         Heading = get_heading()
         stringTmpFile(CD, "CDTempFile")
         tmpFile(Heading, "HeadingTempFile")
-        time.sleep(1)                       # pause
+        time.sleep(0.1)
